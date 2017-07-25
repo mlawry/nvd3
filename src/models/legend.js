4aaa66d2f3ce4f9754df49cc06b4fd7bc522110a
Lawrence Yao <lawry@debian8>
2017-07-25 11:48:43 +1000

    Proper fix for #2, as suggested in upstream issue https://github.com/novus/nvd3/issues/2044

--- src/models/legend.js
+++ src/models/legend.js
@@ -333,11 +333,11 @@ nv.models.legend = function() {
         }
 
         function setBGColor(d,i) {
-            color(d,0); // Temp fix
-            color(d,1); // Temp fix
             if(expanded && vers == 'furious') {
                 return d.disengaged ? '#eee' : d.color || color(d,i);
             } else {
+                if (!d.color)
+                  d.color = color(d,i);
                 return d.color || color(d,i);
             }
         }
